766bbfb1a8cef284494681f9e580dd668e2ee829
Evgeny Abramovich <evg.abramovitch@gmail.com>
2018-02-18 20:53:55 +0300

    Первый рабочий вариант

--- downloader.js
+++ downloader.js
@@ -1,38 +1,115 @@
-const { URLSearchParams } = require('url');
-
-const { ipcRenderer } = require('electron');
+const path = require('path');
+const url = require('url');
+const mime = require('mime-types')
+const { ipcRenderer, remote, BrowserWindow } = require('electron');
+const _ = require('lodash');
+const QueryString = require('querystring');
+const tress = require('tress');
+const fs = require('fs');
+const modal = require('electron-modal');
+const got = require('got');
 const axios = require('axios');
 
-ipcRenderer.on('back', (event, arg) => window.history.back());
-ipcRenderer.on('forward', (event, arg) => window.history.forward());
-ipcRenderer.on('refresh', (event, arg) => window.location.reload());
+const q = tress(function(job, done){
+    job.action(job).then(done);
+}, 3);
 
-ipcRenderer.on('download-all', (event, arg) => {
-    axios.get('/services/books.php', {
-        params: {
-            books_action: 'get_page_info',
-            books_book: 429786,
-            books_page:  1
-        },
-        responseType: 'json'
-    }).then(response => console.log(response.data))
-  
-    var params = new URLSearchParams();
-    params.set('books_action', 'get_page_info');
-    params.set('books_book', 429786);
-    params.set('books_page', 1)
-    console.log(params);
-});
+ipcRenderer.on('back', () => window.history.back());
+ipcRenderer.on('forward', () => window.history.forward());
+ipcRenderer.on('refresh', () => window.location.reload());
 
-ipcRenderer.on('download-range', (event, arg) => {
-    console.log(axios);
+ipcRenderer.on('download-all', async () => {
+    const bookInfo = getBookInformation();
+    if(bookInfo) {
+        const path = await getFolder()
+        for(let i = 1; i <= bookInfo.maxPage; i++) {
+            q.push({
+                action: getBookPageDesc,
+                bookInfo: bookInfo,
+                page: i,
+                path: path
+            });
+        }
+    } else {
+        alert('Не ужаеться найьти книгу на этой старанице');
+    }
 });
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    console.log('DOMContentLoaded')
-    console.log(axios)
-    console.log('DOMContentLoaded')
 });
 
 
-// /services/books.php?books_action=get_page_info&books_book=429786&books_page=1
+function getBookInformation() {
+    const bookId = _.get(QueryString.parse(location.search), 'book_id');
+    const pn_last = _.first(jQuery("#pn_last"));
+    if(bookId && pn_last) {
+        const regexp = /toPage\s*\(\s*(\d*)\s*\)/;
+        const pageMatcher = _.get($(pn_last).data( "events" ), 'click')
+            .map((item) => item.handler.toString().match(regexp))
+            .find((item) => !!item);
+        const maxPage = Number(pageMatcher[1]);
+        return {
+            maxPage: maxPage,
+            bookId: bookId
+        };
+    }
+    return  null;
+}
+
+async function getBookPageDesc(job) {
+    const resourceUrl = new url.URL(url.resolve(location.origin, '/services/books.php'));
+    resourceUrl.search = QueryString.stringify({
+        books_action: 'get_page_info',
+        books_book: job.bookInfo.bookId,
+        books_page:  job.page,
+    });
+
+    try {
+        const response = await new Promise((resolve, reject) => $.ajax(resourceUrl.toString()).then((r) => resolve(JSON.parse(r))));
+        q.push({
+            action: getBookPage,
+            data: response,
+            path: job.path,
+            page:  job.page
+        });
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+async function getBookPage(job) {
+    return axios.get(url.resolve(location.origin, job.data.url), {
+        responseType: 'blob'
+    })
+    .then(function (response) {
+        var fileReader = new FileReader();
+        fileReader.onload = function () {
+            fs.writeFileSync(getFilePath(job.page, job.path), Buffer(new Uint8Array(this.result)))
+        };
+        fileReader.readAsArrayBuffer(response.data);
+    })
+    .catch(function (error) {
+    });
+}
+
+async function getFolder() {
+    return new Promise((resolve) => remote.dialog.showOpenDialog({ properties: ['openDirectory'] }, (files) => resolve(_.first(files))));
+}
+
+function getFilePath(page, dir, headers) {
+    const fileName = ('0000000000' + page.toString()).slice(-10) + '.' + mime.extension('image/png');
+    return path.resolve(dir, fileName);
+}
+
+function promte() {
+    // renderer process (mainWindow)
+    let modal = window.open(path.join(__dirname, 'dialogs/promt.html'), 'modal')
+    console.log(modal)
+
+    let top = new BrowserWindow()
+    let child = new BrowserWindow({parent: top})
+    child.show()
+    top.show()
+}
--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const {
 const path = require('path');
 const axios = require('axios');
 const url = require('url');
+// require('ssl-root-cas/latest')
+//     .inject();
 let mainWindow;
 
 app.on('ready', () => {
@@ -36,19 +38,32 @@ app.on('ready', () => {
         label: 'Скачать всю книгу',
         click: () => mainWindow.webContents.send('download-all', axios)
     }));
-    menu.append(new MenuItem({
-        label: 'Скачать дипазон страниц',
-        click: () => mainWindow.webContents.send('download-range', axios)
-    }));
     mainWindow.setMenu(menu);
-    //https://biblioclub.ru/index.php?page=book_view_red&book_id=429786
-    var initUrl = 'https://biblioclub.ru/index.php?page=main_ub_red&needauth=1'
-    mainWindow.loadURL(initUrl);
+    //mainWindow.loadURL('https://biblioclub.ru/index.php?page=main_ub_red&needauth=1');
+    mainWindow.loadURL('https://biblioclub.ru/index.php?page=book_view_red&book_id=429786');
     mainWindow.webContents.openDevTools();
     mainWindow.on('closed', function () {
         mainWindow = null
     });
-    console.log(axios)
+    mainWindow.webContents.on('new-window', (event, url, frameName, disposition, options, additionalFeatures) => {
+        console.log(event, url, frameName, disposition, options, additionalFeatures);
+        if (frameName === 'modal') {
+            // open window as modal
+            event.preventDefault();
+            Object.assign(options, {
+                modal: true,
+                parent: mainWindow,
+                width: 300,
+                height: 200,
+                frame: false,
+                url: url
+            });
+            const modal = new BrowserWindow(options);
+            modal.setMenu(null);
+            modal.loadURL(url);
+            event.newGuest = modal;
+        }
+    })
 });
 
 app.on('window-all-closed', () => {
